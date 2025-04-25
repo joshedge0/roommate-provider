21e6e1b11eba2a166fbf3ffca2ffee2da9f4c1b8
Josh <josh@Joshs-MacBook-Air.local>
2025-04-25 12:24:11 -0400

    configured husky prelint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
-  <div>
-    <h2>Home</h2>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, dicta ipsum amet quod dolorum iste accusamus molestiae tempora, explicabo eius provident voluptatem delectus, facilis tempore saepe consequuntur perferendis. Esse, voluptates?</p>
-  </div>
+    <div>
+      <h2>Home</h2>
+    </div>
   );
 }
